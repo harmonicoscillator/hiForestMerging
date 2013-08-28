e5a7cb4646e6f8e9bde7eead350a5d4cb255f8a6
R. Alex Barbieri <luck@mit.edu>
2013-08-28 09:39:07 -0400

    Add comments and one check for errors.

--- mergeForest.C
+++ mergeForest.C
@@ -1,11 +1,20 @@
+// mergeForest.C
+// Authors Alex Barbieri and Dragos Velicanu
+// Merge small HiForests into one large HiForest by combining trees.
 
 #include <TChain.h>
 #include <TFile.h>
 #include <TString.h>
 #include <iostream>
 
-void mergeForest(TString fname = "/mnt/hadoop/cms/store/user/luck/test/*.root", TString outfile="output.root")
+void mergeForest(TString fname = "/mnt/hadoop/cms/store/user/luck/test/*.root",
+		 TString outfile="output.root")
 {
+  // First, find on of the files within 'fname' and use it to make a
+  // list of trees. Unfortunately we have to know in advance at least
+  // one of the tree names. hiEvtAnalyzer/HiTree is a safe choice for
+  // HiForests. We also assume that every TTree is inside a
+  // TDirectoryFile which is in the top level of the root file.
   TChain *dummyChain = new TChain("hiEvtAnalyzer/HiTree");
   dummyChain->Add(fname);
   TFile *testFile = dummyChain->GetFile();
@@ -37,31 +46,43 @@ void mergeForest(TString fname = "/mnt/hadoop/cms/store/user/luck/test/*.root", 
 
   testFile->Close();
   delete dummyChain;
-  
-  // for(unsigned i = 0; i < trees.size(); ++i)
-  // {
-  //   std::cout<< trees.at(i) << std::endl;
-  // }
 
+  // Now use the list of tree names to make a new root file, filling
+  // it with the trees from 'fname'.
   const int Ntrees = trees.size();
-  
   TChain* ch[Ntrees];
 
+  Long64_t nentries;
   for(int i = 0; i < Ntrees; ++i){
     ch[i] = new TChain(trees[i]);
     ch[i]->Add(fname);
-    std::cout<<"Tree loaded : " << trees[i] <<std::endl;
-    std::cout<<"Entries : " << ch[i]->GetEntries() <<std::endl;
+    std::cout << "Tree loaded : " << trees[i] << std::endl;
+    std::cout << "Entries : " << ch[i]->GetEntries() << std::endl;
+
+    // If the number of entries in this tree is different from other
+    // trees there is a problem. Quit and inform the user without
+    // producing output.
+    if(i == 0) nentries = ch[i]->GetEntries();
+    else if (nentries != ch[i]->GetEntries())
+    {
+      std::cout << "ERROR: number of entries in this tree does not match." << std::endl;
+      std::cout << "Exiting. Please check input." << std::endl;
+      exit(1);
+    }
   }
 
   TFile* file = new TFile(outfile, "RECREATE");
 
-  for(int i = 0; i < Ntrees; ++i){
+  for(int i = 0; i < Ntrees; ++i)
+  {
     file->cd();
-    std::cout << trees[i] <<std::endl;
-    if (i==0) {
+    std::cout << trees[i] << std::endl;
+    if (i==0)
+    {
       file->mkdir(dir[i])->cd();
-    } else {
+    }
+    else
+    {
       if ( dir[i] != dir[i-1] )
   	file->mkdir(dir[i])->cd();
       else
@@ -69,10 +90,10 @@ void mergeForest(TString fname = "/mnt/hadoop/cms/store/user/luck/test/*.root", 
     }
     ch[i]->Merge(file,0,"keep");
   }
-  std::cout <<"Done"<<std::endl;
   //file->Write();
   file->Close();
 
+  std::cout << "Done. Output: " << outfile << std::endl;
 }
 
 int main(int argc, char *argv[])
