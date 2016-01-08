46ae7b1ec1b27bd553fdbfc36bc1d81732c510ea
R. Alex Barbieri <luck@mit.edu>
2016-01-08 10:43:38 -0500

    switch to hadd for condor script

--- pmerge.sh
+++ pmerge.sh
@@ -21,10 +21,10 @@ echo "$len files, with $njobs jobs"
 cp $1 $now
 mkdir -p $2
 
-echo "compiling merge script"
-g++ mergeForest.C $(root-config --cflags --libs) -Werror -Wall -O2 -o mergeForest.exe || exit 1
+#echo "compiling merge script"
+#g++ mergeForest.C $(root-config --cflags --libs) -Werror -Wall -O2 -o mergeForest.exe || exit 1
 
-cp mergeForest.exe $now
+#cp mergeForest.exe $now
 
 cat > $now/pmerge.condor <<EOF
 Universe     = vanilla
@@ -33,14 +33,14 @@ Notification = Error
 Executable   = $PWD/$now/merge.sh
 Arguments    = \$(Process) $1 $2 $3
 GetEnv       = True
-#Output       = $logdir/\$(Process).out
-#Error        = $logdir/\$(Process).err
-#Log          = $logdir/\$(Process).log
+Output       = \$(Process).out
+Error        = \$(Process).err
+Log          = \$(Process).log
 Rank         = Mips
 +AccountingGroup = "group_cmshi.$(whoami)"
 should_transfer_files = YES
 when_to_transfer_output = ON_EXIT
-transfer_input_files = $1,mergeForest.exe
+transfer_input_files = $1
 
 Queue $njobs
 
@@ -51,7 +51,7 @@ start=\$(((\$1+1)*\$4))
 mkdir mergedTmp
 cat \$2 | head -n \$start | tail -n \$4 | awk -v filename=\$1 -v outdir=\$3 -v nfiles=\$4 '{print "ln -s "\$1" mergedTmp/"}' | bash
 
-echo | awk -v filename=\$1 -v outdir=\$3 -v nfiles=\$4 '{print "./mergeForest.exe \"mergedTmp/*.root\" \""filename".root\""}' | bash
+echo | awk -v filename=\$1 -v outdir=\$3 -v nfiles=\$4 '{print "./mergeForest.exe \""filename".root\" \"mergedTmp/\*.root\""}' | bash
 mv \$1.root \$3
 
 EOF
